ddafeddabd5ce4f90629bc2f65264eb2f48a0d9d
Chloe Horgan <super.chloe@gmail.com>
2016-01-22 13:17:31 -0500

    Initial Commit

---
+++ Finger Paint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Finger Paint
+//
+//  Created by Chloe on 2016-01-22.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
