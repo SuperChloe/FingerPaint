67e5c4b24dc0b7bb2999f8c0a20fc3a98acb7eb3
Chloe Horgan <super.chloe@gmail.com>
2016-01-22 17:14:11 -0500

    Not working

--- Finger Paint/ViewController.m
+++ Finger Paint/ViewController.m
@@ -10,18 +10,53 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIBezierPath *path;
+@property (nonatomic, strong) NSMutableArray *pointsArray;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _path = [UIBezierPath bezierPath];
+    _pointsArray = [[NSMutableArray alloc] init];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)drawRect:(CGRect)rect {
+    [[UIColor blackColor] setStroke];
+    self.path.lineWidth = 3.0;
+    [self.path stroke];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLocation = [touch locationInView:self.view];
+    
+    [self.path moveToPoint:touchLocation];
+    [self.pointsArray addObject:[NSValue valueWithCGPoint:touchLocation]];
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLocaton = [touch locationInView:self.view];
+    
+    [self.path addLineToPoint:touchLocaton];
+    [self.view setNeedsDisplay];
+    [self.pointsArray addObject:[NSValue valueWithCGPoint:touchLocaton]];
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.path closePath];
+}
+
 
 @end
